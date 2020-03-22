11e07b2926058a045de9b42bf9b59f5e8a192940
Igorekmog <m19_bond@tut.by>
2020-03-22 22:49:09 +0300

    feat: complete task cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,68 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    notReverseFlag = true;
+
+    constructor (value) {
+        this.notReverseFlag = value;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(str, key) {
+        const alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        if (!str || !key) {
+            throw new Error();
+        };
+        if (this.notReverseFlag == false) {
+            str = str.split('').reverse().join('');
+        }
+        str = str.toUpperCase();
+        key = key.toUpperCase();
+        let s = "";
+        let j=0;
+            for (let i = 0; i < str.length; i++) {
+                if(!alfabet.includes(str[i])){
+                    s += str[i];
+                    continue;
+                }
+                if(j<key.length){
+                    s += alfabet[(alfabet.indexOf(str[i])+alfabet.indexOf(key[j]))%26];
+                    j++;
+                }
+                else{
+                    j=0;
+                    s += alfabet[(alfabet.indexOf(str[i])+alfabet.indexOf(key[j]))%26];
+                    j++;
+                }
+            }
+            return s;
+    }
+
+    decrypt(str, key) {
+            const alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            if (!str || !key) {
+                throw new Error();
+            };
+            if (this.notReverseFlag == false) {
+                str = str.split('').reverse().join('');
+            }
+            str = str.toUpperCase();
+            key = key.toUpperCase();
+            let s = "";
+            let j=0;
+                for (let i = 0; i < str.length; i++) {
+                    if(!alfabet.includes(str[i])){
+                        s += str[i];
+                        continue;
+                    }
+                    if(j<key.length){
+                        s += alfabet[(alfabet.indexOf(str[i])+26-alfabet.indexOf(key[j]))%26];
+                        j++;
+                    }
+                    else{
+                        j=0;
+                        s += alfabet[(alfabet.indexOf(str[i])+26-alfabet.indexOf(key[j]))%26];
+                        j++;
+                    }
+                }
+                return s;
     }
 }
 
